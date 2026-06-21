5f316ce5630834b97c00d75c73b52a64457274f8
Utkarsh <devspace0201@gmail.com>
2026-06-21 19:03:08 +0530

    feat: update profile picture image asset and set Google Drive resume URL

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -62,7 +62,7 @@ export default function Hero({ onOpenNewsletter }) {
               </svg>
             </a>
             
-            <a href="/resume_placeholder.pdf" target="_blank" rel="noreferrer" className="btn-secondary">
+            <a href="https://drive.google.com/file/d/1FM1_YGj8dEm2JMIy3Vpt-BYweAgiY_6L/view?usp=drive_link" target="_blank" rel="noreferrer" className="btn-secondary">
               View Resume
               <svg width="18" height="18" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
